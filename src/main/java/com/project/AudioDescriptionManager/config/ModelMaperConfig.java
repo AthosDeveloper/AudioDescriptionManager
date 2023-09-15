e5114ad7e1675fb1811ace4c78fb730de115942e
AthosDeveloper <athos.silva@zup.com.br>
2023-09-15 17:33:41 -0300

    initial configuration was enabled

---
+++ src/main/java/com/project/AudioDescriptionManager/config/ModelMaperConfig.java
@@ -0,0 +1,13 @@
+package com.project.AudioDescriptionManager.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMaperConfig {
+    @Bean
+    public ModelMapper mapper(){
+        return  new ModelMapper();
+    }
+}
