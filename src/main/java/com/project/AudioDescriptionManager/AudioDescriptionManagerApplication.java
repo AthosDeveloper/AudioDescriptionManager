4c5c127113b07006b495ba695b8124abf5f1df64
AthosDeveloper <athos.silva@zup.com.br>
2023-09-15 14:26:59 -0300

    initial commit

---
+++ src/main/java/com/project/AudioDescriptionManager/AudioDescriptionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.project.AudioDescriptionManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioDescriptionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioDescriptionManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/AudioDescriptionManager/AudioDescriptionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.AudioDescriptionManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioDescriptionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
